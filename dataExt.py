4544c947ca305a89ff23eaa45b2153577c89d0dc
Sanketh <sankethsequeira@gmail.com>
2023-04-13 20:00:02 +0530

    Initial commit

---
+++ dataExt.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from tkinter import messagebox
+
+def dataExtract(lst):
+    data=pd.read_csv("CET_Database_Final2020.csv")
+    diction=data.to_dict()
+    for i in diction.keys():
+        lst[i]=[]
+        for j in diction[i].values():
+            if not pd.isna(j):
+                lst[i].append(j)
+
+def quota_lst(diction):
+    lst=[]
+    for i in diction.keys():
+        lst.append(i)
+    for i in range(4):
+        lst.pop(0)
+    lst.insert(0,"      --Select-Quota--")
+    return lst
+
+def branch_lst(diction):
+    lst=[]
+    for i in diction["Branch"]:
+        lst.append(i)
+    lst=set(lst)
+    lst=list(lst)
+    lst.insert(0,"      --Select-Branch--")
+    return lst
---
+++ main.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from tkinter import ttk
+from dataExt import *
+
+
+
+#Extracting data from the csv file
+diction={}
+dataExtract(diction)
+
+
+
+#creating the window
+root=tk.Tk()
+root.title("College Predictor")
+root.config(bg="black")
+root.state("zoomed")
+
+
+#creating the form frames
+form=tk.Frame(root, relief="sunken", bg="Black")
+form.pack( expand= False, padx=20, pady=10)
+
+rank=tk.Frame(form,relief="sunken", bg="Black")
+rank.pack(fill=tk.BOTH, expand= True, pady=20)
+
+quota=tk.Frame(form,relief="sunken", bg="Black")
+quota.pack(fill=tk.BOTH, expand= True, pady=20)
+
+branch=tk.Frame(form,relief="sunken", bg="Black")
+branch.pack(fill=tk.BOTH, expand= True, pady=20)
+
+
+
+#Initialising to string variable
+rankVar=tk.StringVar()
+
+
+
+
+#creating rank form elements
+rankLabel=tk.Label(rank, text="Enter your Rank:     ", font=("Times New Roman", 20), fg="white", bg="black")
+rankEntry=tk.Entry(rank, textvariable=rankVar, font=("Times New Roman", 20))
+
+quotaLabel=tk.Label(quota, text="Choose your Quota:     ", font=("Times New Roman", 20), fg="white", bg="black")
+quotaEntry=ttk.Combobox(quota, value=quota_lst(diction))
+quotaEntry.current(0)
+
+branchLabel=tk.Label(branch, text="Choose your Branch:     ", font=("Times New Roman", 20), fg="white", bg="black")
+branchEntry=ttk.Combobox(branch, value=branch_lst(diction))
+branchEntry.current(0)
+
+
+
+
+#placing the form elements
+rankLabel.pack(side=tk.LEFT)
+rankEntry.pack(side=tk.RIGHT)
+
+quotaLabel.pack(side=tk.LEFT)
+quotaEntry.pack(side=tk.LEFT)
+
+branchLabel.pack(side=tk.LEFT)
+branchEntry.pack(side=tk.LEFT)
+
+
+
+
+
+root.mainloop()
